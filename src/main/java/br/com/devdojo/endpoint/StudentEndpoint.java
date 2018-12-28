db7a90efa3bcaa1b53291aee4cf884cef9959e32
Thiago Ericson <ericson.thiago@gmail.com>
2018-12-28 14:43:36 -0200

    Spring Boot Essentials 04 - Setup do projeto com Spring Initializr

---
+++ src/main/java/br/com/devdojo/endpoint/StudentEndpoint.java
@@ -0,0 +1,24 @@
+package br.com.devdojo.endpoint;
+
+import br.com.devdojo.model.Student;
+import br.com.devdojo.util.DateUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+import static java.util.Arrays.asList;
+
+@RestController
+@RequestMapping("student")
+public class StudentEndpoint {
+    @Autowired
+    private DateUtil dateUtil;
+    @RequestMapping(method = RequestMethod.GET, path = "/list")
+    public List<Student> listAll(){
+        //System.out.println(dateUtil.formatLocalDateTimeFormatToDatabaseStyle(LocalDateTime.now()));
+        return asList(new Student("Deku"), new Student("Todoroki"));
+    }
+}
