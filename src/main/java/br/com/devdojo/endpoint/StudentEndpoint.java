6ea564615c4dea7540c497c73468b061bc3efe51
Thiago Ericson <ericson.thiago@gmail.com>
2018-12-29 10:45:00 -0200

    Spring Boot Essentials 07 - Padrões REST e PUT e DELETE pt 03

--- src/main/java/br/com/devdojo/endpoint/StudentEndpoint.java
+++ src/main/java/br/com/devdojo/endpoint/StudentEndpoint.java
@@ -22,13 +22,13 @@ public class StudentEndpoint {
         this.dateUtil = dateUtil;
     }
 
-    @RequestMapping(method = RequestMethod.GET)
+    @GetMapping
     public ResponseEntity<?> listAll(){
-//        System.out.println(dateUtil.formatLocalDateTimeFormatToDatabaseStyle(LocalDateTime.now()));
+//        System.out.println("-----------"+dateUtil.formatLocalDateTimeFormatToDatabaseStyle(LocalDateTime.now()));
         return new ResponseEntity<>(Student.studentList, HttpStatus.OK);
     }
 
-    @RequestMapping(method = RequestMethod.GET, path = "/{id}")
+    @GetMapping(path = "/{id}")
     public ResponseEntity<?> getStudentById(@PathVariable("id") int id) {
         Student student = new Student();
         student.setId(id);
@@ -38,9 +38,22 @@ public class StudentEndpoint {
         return new ResponseEntity<>(Student.studentList.get(index), HttpStatus.OK);
     }
 
-    @RequestMapping(method = RequestMethod.POST)
+    @PostMapping
     public ResponseEntity<?> save(@RequestBody Student student) {
         Student.studentList.add(student);
         return new ResponseEntity<>(student, HttpStatus.OK);
+    }
+
+    @DeleteMapping
+    public ResponseEntity<?> delete(@RequestBody Student student) {
+        Student.studentList.remove(student);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @PutMapping
+    public ResponseEntity<?> update(@RequestBody Student student) {
+        Student.studentList.remove(student);
+        Student.studentList.add(student);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
