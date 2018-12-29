1560474fb638ad7796be80e6135ead373bb8b228
Thiago Ericson <ericson.thiago@gmail.com>
2018-12-29 10:13:25 -0200

    Spring Boot Essentials 05 - Padrões REST e POST pt 01

--- src/main/java/br/com/devdojo/endpoint/StudentEndpoint.java
+++ src/main/java/br/com/devdojo/endpoint/StudentEndpoint.java
@@ -1,8 +1,12 @@
 package br.com.devdojo.endpoint;
 
+import br.com.devdojo.error.CustomErrorType;
 import br.com.devdojo.model.Student;
 import br.com.devdojo.util.DateUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,13 +16,28 @@ import java.util.List;
 import static java.util.Arrays.asList;
 
 @RestController
-@RequestMapping("student")
+@RequestMapping("students")
 public class StudentEndpoint {
+    private final DateUtil dateUtil;
+
     @Autowired
-    private DateUtil dateUtil;
-    @RequestMapping(method = RequestMethod.GET, path = "/list")
-    public List<Student> listAll(){
-        //System.out.println(dateUtil.formatLocalDateTimeFormatToDatabaseStyle(LocalDateTime.now()));
-        return asList(new Student("Deku"), new Student("Todoroki"));
+    public StudentEndpoint(DateUtil dateUtil) {
+        this.dateUtil = dateUtil;
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public ResponseEntity<?> listAll(){
+//        System.out.println(dateUtil.formatLocalDateTimeFormatToDatabaseStyle(LocalDateTime.now()));
+        return new ResponseEntity<>(Student.studentList, HttpStatus.OK);
+    }
+
+    @RequestMapping(method = RequestMethod.GET, path = "/{id}")
+    public ResponseEntity<?> getStudentById(@PathVariable("id") int id) {
+        Student student = new Student();
+        student.setId(id);
+        int index = Student.studentList.indexOf(student);
+        if(index == -1)
+            return new ResponseEntity<>(new CustomErrorType("Student Not Found"), HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>(Student.studentList.get(index), HttpStatus.OK);
     }
 }
