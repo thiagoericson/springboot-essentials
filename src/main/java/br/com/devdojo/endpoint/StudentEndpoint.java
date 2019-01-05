bcdd1f3491e85e575d2f887d1b1df44888ababe4
Thiago Ericson <ericson.thiago@gmail.com>
2019-01-05 21:17:30 -0200

    Spring Boot Essentials 17 - Spring Security pt 01 - Autenticação e autorização

--- src/main/java/br/com/devdojo/endpoint/StudentEndpoint.java
+++ src/main/java/br/com/devdojo/endpoint/StudentEndpoint.java
@@ -8,6 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
@@ -34,7 +37,9 @@ public class StudentEndpoint {
     }
 
     @GetMapping(path = "/{id}")
-    public ResponseEntity<?> getStudentById(@PathVariable("id") Long id) {
+    public ResponseEntity<?> getStudentById(@PathVariable("id") Long id,
+                                            @AuthenticationPrincipal UserDetails userDetails) {
+        System.out.println(userDetails);
         verifyIfStudentExists(id);
         Student student = studentDAO.findById(id).orElse(null);
         return new ResponseEntity<>(student, HttpStatus.OK);
@@ -52,6 +57,7 @@ public class StudentEndpoint {
     }
 
     @DeleteMapping(path = "/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<?> delete(@PathVariable Long id) {
         verifyIfStudentExists(id);
         studentDAO.deleteById(id);
