c57e8f1af93df18116fcb039675c2a284b70ab3d
Thiago Ericson <ericson.thiago@gmail.com>
2019-01-05 23:16:31 -0200

    Spring Boot Essentials 18 - Spring Security pt 02 - Autenticação e autorização com Spring Data

--- src/main/java/br/com/devdojo/config/SecurityConfig.java
+++ src/main/java/br/com/devdojo/config/SecurityConfig.java
@@ -1,16 +1,20 @@
 package br.com.devdojo.config;
 
+import br.com.devdojo.service.CustomUserDetailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private CustomUserDetailService customUserDetailService;
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
@@ -21,11 +25,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .csrf().disable();
     }
 
-    @Autowired
-    public void configGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("teste").password("{noop}devdojo").roles("USER")
-                .and()
-                .withUser("admin").password("{noop}devdojo").roles("USER", "ADMIN");
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(customUserDetailService).passwordEncoder(new BCryptPasswordEncoder());
     }
+
+    //    @Autowired
+//    public void configGlobal(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.inMemoryAuthentication()
+//                .withUser("teste").password("{noop}devdojo").roles("USER")
+//                .and()
+//                .withUser("admin").password("{noop}devdojo").roles("USER", "ADMIN");
+//    }
 }
